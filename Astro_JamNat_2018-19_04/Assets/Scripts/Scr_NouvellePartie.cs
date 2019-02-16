fa1b5365ae49e8791bceb268a431506d5f69d1bd
Unknown <eve.gaboury@gmail.com>
2019-02-16 15:39:54 -0500

    Before merge 2 scenes

--- Astro_JamNat_2018-19_04/Assets/Scripts/Scr_NouvellePartie.cs
+++ Astro_JamNat_2018-19_04/Assets/Scripts/Scr_NouvellePartie.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Scr_NouvellePartie : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Button enter;
+    public TMP_InputField InputField;
+    public int nbrPlayer;
+    public GameObject playerIcon;
+    public GridLayoutGroup playerGroup;
+    public Button StartButton;
+
+
+    // Use this for initialization
+    void Start () {
+
 		
 	}
 	
@@ -13,4 +25,24 @@ public class Scr_NouvellePartie : MonoBehaviour {
 	void Update () {
 		
 	}
-}
+
+    public void NbrPlayer () {
+
+        int.TryParse(InputField.text, out nbrPlayer);
+    
+    }
+
+    public void InstancePlayerIcon () {
+
+        for (int i = 0; i < nbrPlayer; i++) {
+            Instantiate(playerIcon, playerGroup.transform);
+        }
+
+        StartButton.gameObject.SetActive(true);
+    }
+
+    public void LoadScene () {
+
+        SceneManager.LoadScene("Match");
+    }
+} //fin class
