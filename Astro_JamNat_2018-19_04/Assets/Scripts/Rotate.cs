872deb618af58b09914fa8acbc24289611fb53cd
Manuel <manuel.bergeron.d@gmail.com>
2019-02-16 22:13:03 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Astro_JamNat_2018-19_04/Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float rotateSpeed = 1.0f;
+    public Transform ADN;
+
+    // Update is called once per frame
+    void Update () {
+		
+        ADN.Rotate(Vector3.right * Time.deltaTime * rotateSpeed);
+
+    }
+}
