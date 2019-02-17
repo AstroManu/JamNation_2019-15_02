9936198a8482258aed157f7482e95cdd1ecab2b8
Unknown <eve.gaboury@gmail.com>
2019-02-17 13:01:54 -0500

    New cards page
    wowowowowow

---
+++ Astro_JamNat_2018-19_04/Assets/Scripts/EvolutionCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolutionCard : MonoBehaviour {
+
+
+    public int index = -1;
+
+
+    public void OnPress()
+    {
+
+
+        for (int i = 0; i < transform.parent.childCount; i++) {
+
+
+            if (transform.parent.GetChild(i) == transform)
+                index = i;
+        }
+
+            Scr_ChoosingCards.instance.InstanceChosenCards(index);
+
+    }
+}
